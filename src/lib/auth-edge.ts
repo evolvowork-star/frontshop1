05c98c6202a6ed2abad4d377a089dc08f137ad79
evolvowork-star <evolvowork@gmail.com>
2026-05-22 16:41:18 +0500

    final 3

--- src/lib/auth-edge.ts
+++ src/lib/auth-edge.ts
@@ -1,11 +1,13 @@
-// src/lib/auth-edge.ts
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 import { getToken } from "next-auth/jwt"
 
 export async function auth(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
+    cookieName: process.env.NODE_ENV === "production"
+      ? "__Secure-authjs.session-token"
+      : "authjs.session-token",
   })
   return token
 }
